22782407c7d5d92b08dbdb09ad6e3da70a9a8800
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-10 13:13:54 +0200

    Updates

---
+++ precache-manifest.71b067db9feefcfb3aaf18a6e988264e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c832a8d658ef68d4d69967e1f59509",
+    "url": "/soundbar/index.html"
+  },
+  {
+    "revision": "cfdd0649bf592d088560",
+    "url": "/soundbar/static/css/main.5d63e73e.chunk.css"
+  },
+  {
+    "revision": "9a6f76eb9747b2b1169a",
+    "url": "/soundbar/static/js/2.610f527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/soundbar/static/js/2.610f527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdd0649bf592d088560",
+    "url": "/soundbar/static/js/main.cb9e3d25.chunk.js"
+  },
+  {
+    "revision": "0685fbc84010f56dce9a",
+    "url": "/soundbar/static/js/runtime-main.262b97eb.js"
+  },
+  {
+    "revision": "1b35738edf3e1dc4c5d7608590e1b5b5",
+    "url": "/soundbar/static/media/martellone.1b35738e.mp3"
+  },
+  {
+    "revision": "b3a284d0a4a216b4b0d8f395d3fe89a0",
+    "url": "/soundbar/static/media/patapum.b3a284d0.mp3"
+  },
+  {
+    "revision": "f7c40d63d46f2487e44336f840f3809e",
+    "url": "/soundbar/static/media/quark.f7c40d63.mp3"
+  },
+  {
+    "revision": "69d3ca94835174471f96e219677e32d6",
+    "url": "/soundbar/static/media/sadtrombone.69d3ca94.mp3"
+  }
+]);
